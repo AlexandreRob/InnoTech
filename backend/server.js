14f55206f8e3f82a62ed4982f3cb9aae04a4066e
AlexandreRob <alexandre.robert974@hotmail.fr>
2025-04-30 16:29:55 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const connectMongo = require("./db/mongo");
+const pg = require("./db/pg");
 
 const app = express();
 app.use(cors());
@@ -9,5 +11,20 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 app.use("/api", require("./routes/index"));
+app.use("/api/users_mongo", require("./routes/users.mongo"));
+app.use("/api/users_pg", require("./routes/users.pg"));
+
+// Lancer la connexion Mongo au démarrage
+connectMongo();
+
+// Exemple test PostgreSQL (optionnel)
+pg.query("SELECT NOW()")
+  .then((res) => {
+    console.log("🗃️ PostgreSQL connecté à", res.rows[0].now);
+  })
+  .catch(console.error);
+
+const healthRoute = require("./routes/health");
+app.use("/api", healthRoute);
 
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
